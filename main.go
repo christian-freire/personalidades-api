0465596e021c14adfecf5cf6662d1fffad6f7efc
Christian Evangelista Freire <74272121+christian-freire@users.noreply.github.com>
2023-04-19 17:56:52 -0300

    [feat] Connected to DataBase running in Docker

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api-go-rest/database"
 	"api-go-rest/models"
 	"api-go-rest/routes"
 	"fmt"
@@ -16,6 +17,8 @@ func main() {
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
+
+	database.ConectaComBancoDeDados()
 
 	fmt.Println("Iniciando API em Go")
 	routes.HandleRequest()
